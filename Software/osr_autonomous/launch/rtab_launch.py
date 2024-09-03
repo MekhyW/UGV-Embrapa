7ea6009bcfd9745d39f05538ed182c9b8c687bfd
rafakatri <rkatri20033@gmail.com>
2024-09-03 13:26:12 -0300

    RTAB-MAP launch: imu + rgbd sync options

--- Software/osr_autonomous/launch/rtab_launch.py
+++ Software/osr_autonomous/launch/rtab_launch.py
@@ -15,13 +15,17 @@ def generate_launch_description():
         IncludeLaunchDescription(
             rtabmap_launch_file,
             launch_arguments={
-                'rgb_topic': '/camera/camera/color/image_rect_raw',
-                'depth_topic': '/camera/camera/depth/image_rect_raw',
+                'rgb_topic': '/camera/camera/color/image_raw',
+                'depth_topic': '/camera/camera/aligned_depth_to_color/image_raw',
                 'camera_info_topic': '/camera/camera/color/camera_info',
-                'imu_topic': '/imu/data',
+                #'rgbd_topic': '/camera/camera/rgbd',
+                #'rgbd_sync': 'true',
+                'qos': '1',
+                'rviz': 'true',
+                'frame_id': 'camera_link',
+                'imu_topic': '/camera/camera/imu',
                 #'subscribe_scan': 'true',
                 #'scan_topic': '/scan',
-                #'rgbd_sync': 'true',
             }.items()
         ),
     ])
