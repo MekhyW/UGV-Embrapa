09ccb41d5acafa99e6941f5efb5c6a3cffc44343
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-10-15 22:04:13 -0300

    Sphinx workflow

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'UGV Embrapa'
+copyright = '2024, Felipe Catapano Emrich Melo, Rafael Eli Katri, Gabriel Brunoro Motta Tumang, Luana de Matos Sorpreso'
+author = 'Felipe Catapano Emrich Melo, Rafael Eli Katri, Gabriel Brunoro Motta Tumang, Luana de Matos Sorpreso'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
